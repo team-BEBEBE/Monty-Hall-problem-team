4bd3937bc7bdca530be6dcb1ee3152a2868012c2
1jeongg <leena0912@pusan.ac.kr>
2023-05-01 20:02:39 +0900

    feat: first open door

--- monty_hall.py
+++ monty_hall.py
@@ -1,8 +1,12 @@
 from random import randint
 
+def except_and_random_choice(ex1, ex2):
+    return ex1
+
 def monty_hall():
     car = randint(0, 2)
     first_choice = randint(0, 2)
+    open_door = except_and_random_choice(car, first_choice)
     return True, True
 
 print(monty_hall())
