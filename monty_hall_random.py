d4a1492d39355f841296c2726d9d2fe98d5ccd4a
1jeongg <leena0912@pusan.ac.kr>
2023-05-01 20:43:37 +0900

    fix: remove duplicated (input) car, door input duplicated removed

--- monty_hall_random.py
+++ monty_hall_random.py
@@ -1,11 +1,19 @@
 from random import randint
 
+def monty_hall_input() -> (int, int):
+    car: int = randint(0, 2)
+    first_choice: int = randint(0, 2)
+    return car, first_choice
+
 def except_and_random_choice(ex1, ex2):
-    return ex1
+    chosen = ex1
+    while chosen in (ex1, ex2):
+        chosen = randint(0,2)
+
+    return chosen 
 
 def monty_hall():
-    car = randint(0, 2)
-    first_choice = randint(0, 2)
+    car, first_choice = monty_hall_input()
     open_door = except_and_random_choice(car, first_choice)
     second_choice = except_and_random_choice(first_choice, open_door)
     return car==first_choice, car==second_choice
