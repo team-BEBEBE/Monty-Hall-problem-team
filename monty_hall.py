191d1a32c67ca01a1f94a080ac01451ebe1a2c56
1jeongg <leena0912@pusan.ac.kr>
2023-05-01 20:04:17 +0900

    feat: return if you open the door that has car

--- monty_hall.py
+++ monty_hall.py
@@ -8,6 +8,6 @@ def monty_hall():
     first_choice = randint(0, 2)
     open_door = except_and_random_choice(car, first_choice)
     second_choice = except_and_random_choice(first_choice, open_door)
-    return True, True
+    return car==first_choice, car==second_choice
 
 print(monty_hall())
